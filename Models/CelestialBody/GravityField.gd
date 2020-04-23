839562d6f992c13193daebc5dd4a941c927d8c3d
Adam Edlund <adam92edlund@gmail.com>
2020-04-23 22:28:58 +0200

    Changed gravity

---
+++ Models/CelestialBody/GravityField.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _ready():
+	set_gravity_distance_scale(0.001)
+	set_gravity_is_point(true)
+	set_gravity_vector(Vector2.ZERO)
+	set_linear_damp(0.0)
+	set_angular_damp(0.0)
+	space_override = Area2D.SPACE_OVERRIDE_COMBINE
